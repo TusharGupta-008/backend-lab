d3f1c7c41b2b4a1f2ac68e2b4d626a6658a6199d
TusharGupta-008 <tushargupta.dsce@gmail.com>
2026-05-15 14:40:54 +0530

    dotenv files added

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config()
 
 const app = express()
+const port = process.env.PORT || 7000
 
 app.get("/",(req,res)=>{
-  res.send("Hiiii")
+  res.send("Hiiii im on the way")
 })
 
-const PORT = 3000;
-app.listen(PORT,()=>{
-  console.log("Server is running")
+app.listen(port,()=>{
+  console.log(`Server is running on ${port}`)
 })
