0f6280eb476d242b5e23db249bb1f417f07bd433
TusharGupta-008 <tushargupta.dsce@gmail.com>
2026-05-16 19:08:40 +0530

    making model for login

--- models/user.model.js
+++ models/user.model.js
@@ -1,2 +1,38 @@
 import mongoose from "mongoose";
 
+const userSchema = new mongoose.Schema(
+  {
+    firsName: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    userName: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+const User = mongoose.model("user", userSchema);
+
+export default User
